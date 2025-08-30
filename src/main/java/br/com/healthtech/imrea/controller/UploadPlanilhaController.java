b18b3ec00744c3b59ef3885998645633de4a4c56
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-29 23:14:14 -0300

    Incluindo requisição POST

--- src/main/java/br/com/healthtech/imrea/controller/UploadPlanilhaController.java
+++ src/main/java/br/com/healthtech/imrea/controller/UploadPlanilhaController.java
@@ -1,8 +1,12 @@
 package br.com.healthtech.imrea.controller;
 
+import br.com.healthtech.imrea.domain.FormData;
 import br.com.healthtech.imrea.service.UploadPlanilhaService;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/api/upload")
@@ -14,9 +18,20 @@ public class UploadPlanilhaController {
         this.uploadPlanilhaService = uploadPlanilhaService;
     }
 
+    /* COMENTANDO UPLOAD LOCAL
     @GET
     public Response getUploadPlanilha() {
         uploadPlanilhaService.processarPlanilha();
+        return Response.ok().build();
+    }
+    COMENTANDO UPLOAD LOCAL */
+
+    @POST
+    @Path("/receber")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response receberArquivo(FormData arquivoRecebido) {
+
+        uploadPlanilhaService.processarPlanilha(arquivoRecebido.fileUpload);
         return Response.ok().build();
     }
 }
--- src/main/java/br/com/healthtech/imrea/service/UploadPlanilhaService.java
+++ src/main/java/br/com/healthtech/imrea/service/UploadPlanilhaService.java
@@ -4,6 +4,7 @@ import br.com.healthtech.imrea.domain.RegistroAgendamento;
 import br.com.healthtech.imrea.domain.UploadLog;
 import com.alibaba.excel.EasyExcel;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,35 +16,55 @@ public class UploadPlanilhaService {
 
     private static final Logger logger = LoggerFactory.getLogger(UploadPlanilhaService.class);
 
-    public void processarPlanilha() {
+    public void processarPlanilha(FileUpload fileUpload) {
 
         String nomeArquivo = "PlanilhaFicticia.xlsx";
         //UploadLog uploadLog = new UploadLog(1L, "Planilha", Date.from(Instant.now()));
         logger.info("Processando planilha do arquivo {}", nomeArquivo);
         try {
             if (nomeArquivo.toLowerCase().endsWith(".xlsx")) {
-                processarXlsx();
+                processarXlsx(fileUpload.uploadedFile().toFile());
             } else {
             }
 
         } catch (Exception e) {
-            logger.info("Erro ao processar planilha");
+            logger.error("Erro ao processar planilha");
+            logger.error(e.getMessage());
         }
     }
-
+    /* COMENTANDO UPLOAD LOCAL
     public void processarXlsx()  {
-        String arquivoXLSX = "C:\\Dev\\Planilha fictícia de acompanhamento.xlsx";
+        String arquivoXLSX = "C:\\Devv\\Planilha fictícia de acompanhamento.xlsx";
         List<RegistroAgendamento> listasAgendamentos = EasyExcel.read(new File(arquivoXLSX))
-                                                                .head(RegistroAgendamento.class)
-                                                                .sheet()
-                                                                .doReadSync();
+                .head(RegistroAgendamento.class)
+                .sheet()
+                .doReadSync();
 
         logger.info("Total de produtos lidos: " + listasAgendamentos.size());
         for (RegistroAgendamento registroAgendamento : listasAgendamentos) {
             logger.info("Nome do paciente: "+registroAgendamento.getNomePaciente()+" - Nome do médico: "+registroAgendamento.getNomeMedico()
-            + " - Data agendada: "+registroAgendamento.getDataAgendamento()+" às " +registroAgendamento.getHoraAgendamento());
+                    + " - Data agendada: "+registroAgendamento.getDataAgendamento()+" às " +registroAgendamento.getHoraAgendamento());
         }
     }
+    COMENTANDO UPLOAD LOCAL */
 
+    public void processarXlsx(File planilha)  {
+        List<RegistroAgendamento> listasAgendamentos;
+        try {
+            listasAgendamentos = EasyExcel.read(planilha)
+                    .head(RegistroAgendamento.class)
+                    .sheet()
+                    .doReadSync();
+
+            logger.info("Total de registros lidos: " + listasAgendamentos.size());
+            for (RegistroAgendamento registroAgendamento : listasAgendamentos) {
+                logger.info("Nome do paciente: "+registroAgendamento.getNomePaciente()+" - Nome do médico: "+registroAgendamento.getNomeMedico()
+                        + " - Data agendada: "+registroAgendamento.getDataAgendamento()+" às " +registroAgendamento.getHoraAgendamento());
+            }
+
+        } catch (Exception e) {
+            logger.error("Erro ao processar a planilha: " + e.getMessage(), e);
+        }
+    }
 
 }
