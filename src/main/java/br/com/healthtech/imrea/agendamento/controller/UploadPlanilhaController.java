d7a018d96544eed38ba7f64afcd67133e2611fb6
mrpine19 <mrpine19@gmail.com>
2025-09-14 22:25:57 -0300

    Ajustando funcionalidade de upload para integração com front

--- src/main/java/br/com/healthtech/imrea/agendamento/controller/UploadPlanilhaController.java
+++ src/main/java/br/com/healthtech/imrea/agendamento/controller/UploadPlanilhaController.java
@@ -7,6 +7,7 @@ import br.com.healthtech.imrea.agendamento.service.UploadPlanilhaService;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -24,8 +25,23 @@ public class UploadPlanilhaController {
     @POST
     @Path("/receber")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response receberArquivo(FormData arquivoRecebido) {
-        UploadDTO uploadDTO = uploadPlanilhaService.processarPlanilha(arquivoRecebido.fileUpload);
-        return Response.ok(uploadDTO).build();
+        List<RegistroAgendamento> listasAgendamentos = uploadPlanilhaService.processarPlanilha(arquivoRecebido.fileUpload);
+        return Response.ok(listasAgendamentos).build();
+    }
+
+    @POST
+    @Path("/salvar")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response salvarDadosAgendamento(List<RegistroAgendamento> agendamentos)  {
+        try{
+            uploadPlanilhaService.salvarDadosAgendamento(agendamentos);
+            return Response.ok().build();
+        }catch (Exception e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Erro ao salvar os dados: " + e.getMessage())
+                    .build();
+        }
     }
 }
