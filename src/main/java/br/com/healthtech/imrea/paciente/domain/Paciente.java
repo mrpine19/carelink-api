13bdd9dfc9079ba39ae53490c67f5e8ca32866cf
mrpine19 <mrpine19@gmail.com>
2025-09-20 20:49:38 -0300

    Criando a entidade cuidador e salvando as informações no banco e envio do alerta

--- src/main/java/br/com/healthtech/imrea/paciente/domain/Paciente.java
+++ src/main/java/br/com/healthtech/imrea/paciente/domain/Paciente.java
@@ -4,6 +4,9 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="TB_CAR_PACIENTE")
@@ -15,14 +18,20 @@ public class Paciente extends PanacheEntityBase {
     @Column(name="id_paciente")
     public Long idPaciente;
 
+    @ManyToMany
+    @JoinTable(name = "TB_CAR_PACIENTE_CUIDADOR",
+            joinColumns = @JoinColumn(name = "id_paciente"),
+            inverseJoinColumns = @JoinColumn(name = "id_cuidador"))
+    public Set<Cuidador> cuidadores = new HashSet<>();
+
     @Column(name="nome_paciente")
     public String nomePaciente;
 
     @Column(name="celular_paciente")
     public String telefonePaciente;
 
-    @Column(name="idade_paciente")
-    public int idadePaciente;
+    @Column(name="data_nascimento_paciente")
+    public Date dataNascimentoPaciente;
 
     @Column(name="dt_criacao")
     public LocalDateTime dtCriacaoPaciente;
@@ -30,8 +39,9 @@ public class Paciente extends PanacheEntityBase {
     public Paciente() {
     }
 
-    public Paciente(String nomePaciente, String telefonePaciente) {
+    public Paciente(String nomePaciente, String telefonePaciente, Date dataNascimentoPaciente) {
         this.nomePaciente = nomePaciente;
         this.telefonePaciente = telefonePaciente;
+        this.dataNascimentoPaciente = dataNascimentoPaciente;
     }
 }
---
+++ src/main/java/br/com/healthtech/imrea/paciente/service/CuidadorService.java
@@ -0,0 +1,32 @@
+package br.com.healthtech.imrea.paciente.service;
+
+import br.com.healthtech.imrea.paciente.domain.Cuidador;
+import br.com.healthtech.imrea.paciente.domain.Paciente;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+
+@ApplicationScoped
+public class CuidadorService {
+    private static final Logger logger = LoggerFactory.getLogger(CuidadorService.class);
+
+    @Transactional
+    public Cuidador buscarOuCriarCuidador(Cuidador cuidador){
+        if (cuidador.nomeCuidador == null || cuidador.nomeCuidador.isEmpty())
+            return null;
+
+        Cuidador cuidadorExistente = Cuidador.find("nomeCuidador = ?1 and telefoneCuidador = ?2", cuidador.nomeCuidador, cuidador.telefoneCuidador).firstResult();
+        if (cuidadorExistente == null){
+            cuidador.dtCriacaoCuidador = LocalDateTime.now();
+            cuidador.persist();
+            logger.info("Cuidador {} salvo com sucesso!", cuidador.nomeCuidador);
+            return cuidador;
+        }else{
+            logger.info("Cuidador {} já existe!", cuidador.nomeCuidador);
+            return cuidadorExistente;
+        }
+    }
+}
