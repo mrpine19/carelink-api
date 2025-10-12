567ce274ed6d1531e1d8fe49f39d8ec6ce83900c
mrpine19 <mrpine19@gmail.com>
2025-10-12 19:25:25 -0300

    Criando endpoint para salvar anotações sobre os pacientes

--- src/main/java/br/com/healthtech/imrea/interacao/service/AnotacaoManualService.java
+++ src/main/java/br/com/healthtech/imrea/interacao/service/AnotacaoManualService.java
@@ -1,14 +1,25 @@
 package br.com.healthtech.imrea.interacao.service;
 
 import br.com.healthtech.imrea.interacao.domain.AnotacaoManual;
+import br.com.healthtech.imrea.interacao.dto.AnotacaoInputDTO;
 import br.com.healthtech.imrea.interacao.dto.InteracaoEquipeDTO;
+import br.com.healthtech.imrea.paciente.domain.Paciente;
+import br.com.healthtech.imrea.paciente.service.PacienteService;
+import br.com.healthtech.imrea.usuario.domain.Usuario;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
 public class AnotacaoManualService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AnotacaoManualService.class);
 
     public List<InteracaoEquipeDTO> buscarHistoricoEquipePorPaciente(Long idPaciente) {
         List<AnotacaoManual> anotacoes = AnotacaoManual.find("paciente.idPaciente = ?1 order by dataHoraAnotacao desc", idPaciente).list();
@@ -25,5 +36,40 @@ public class AnotacaoManualService {
             historico.add(interacao);
         }
         return historico;
+    }
+
+    @Transactional
+    public void salvarAnotacao(AnotacaoInputDTO anotacaoInputDTO) {
+        if (anotacaoInputDTO.getIdPaciente() == null || anotacaoInputDTO.getIdPaciente() <= 0){
+            logger.warn("Id do paciente inválido: {}", anotacaoInputDTO.getIdPaciente());
+            throw new BadRequestException("Id do paciente inválido");
+        }
+
+        Paciente paciente = Paciente.findById(anotacaoInputDTO.getIdPaciente());
+        if (paciente == null){
+            logger.warn("Paciente não encontrado: {}", anotacaoInputDTO.getIdPaciente());
+            throw new NotFoundException("Paciente não encontrado");
+        }
+
+        if (anotacaoInputDTO.getIdUsuario() == null || anotacaoInputDTO.getIdUsuario() <= 0){
+            logger.warn("Id do usuário inválido: {}", anotacaoInputDTO.getIdUsuario());
+            throw new BadRequestException ("Id do usuário inválido");
+        }
+
+        Usuario usuario = Usuario.findById(anotacaoInputDTO.getIdUsuario());
+
+        if (usuario == null){
+            logger.warn("Usuário não encontrado: {}", anotacaoInputDTO.getIdUsuario());
+            throw new NotFoundException("Usuário não encontrado");
+        }
+
+        if (anotacaoInputDTO.getConteudoAnotacao() == null || anotacaoInputDTO.getConteudoAnotacao().isEmpty()){
+            logger.warn("Conteúdo da anotação inválido");
+            throw new BadRequestException("Conteúdo da anotação inválido");
+        }
+
+        AnotacaoManual anotacaoManual = new AnotacaoManual(paciente, usuario, anotacaoInputDTO.getConteudoAnotacao());
+        anotacaoManual.persist();
+        logger.info("Anotação manual salva com sucesso para o paciente {} pelo usuário {}", paciente.nomePaciente, usuario.nomeUsuario);
     }
 }
