2ba4b2370fa22824b4597fc67eb832dbc9277596
mrpine19 <mrpine19@gmail.com>
2025-10-12 22:15:49 -0300

    Criando endpoint para editar anotações sobre os pacientes

--- src/main/java/br/com/healthtech/imrea/interacao/controller/AnotacaoManualController.java
+++ src/main/java/br/com/healthtech/imrea/interacao/controller/AnotacaoManualController.java
@@ -1,10 +1,12 @@
 package br.com.healthtech.imrea.interacao.controller;
 
 import br.com.healthtech.imrea.interacao.dto.AnotacaoInputDTO;
+import br.com.healthtech.imrea.interacao.dto.AnotacaoUpdateDTO;
 import br.com.healthtech.imrea.interacao.service.AnotacaoManualService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -20,6 +22,14 @@ public class AnotacaoManualController {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response salvarAnotacao(AnotacaoInputDTO anotacaoInputDTO) {
         anotacaoManualService.salvarAnotacao(anotacaoInputDTO);
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path("")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response alterarAnotacao(AnotacaoUpdateDTO anotacaoUpdateDTO) {
+        anotacaoManualService.alterarAnotacao(anotacaoUpdateDTO);
         return Response.ok().build();
     }
 }
